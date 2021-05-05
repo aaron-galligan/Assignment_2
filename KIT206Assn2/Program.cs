c04b1577ad23fcd6f9acfb9197a82fd9a751e347
Aaron Galligan <azza.galligan@gmail.com>
2021-05-05 20:31:47 +1000

    Test to make sure i can upload files

---
+++ KIT206Assn2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KIT206Assn2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ KIT206Assn2/Researcher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KIT206Assn2
+{
+    class Researcher
+    {
+    }
+}
